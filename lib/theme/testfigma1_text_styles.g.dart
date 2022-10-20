3d4c87dd4ede602ef90ae1fe4503904633a93c99
Parabeac-Nest <admin@parabeac.com>
2022-10-20 02:07:56 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/testfigma1_text_styles.g.dart
@@ -0,0 +1,70 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class Testfigma1TextStyles {
+
+static const TextStyle tINYLETTERTinyLetterRegular3 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'DB HelvethaicaMon X',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle tITLETitle6 = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'DB HelvethaicaMon X',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle tINYLETTERTinyLetterRegular2 = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'DB HelvethaicaMon X',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle tINYLETTERTinyLetterRegular5 = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'DB HelvethaicaMon X',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle defaultBoldSubheadline = TextStyle(
+  fontSize: 15,
+  fontWeight: FontWeight.w600,
+  letterSpacing: -0.5,
+  fontFamily: 'SF Pro Text',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle tINYLETTERTinyLetterBold3 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'DB HelvethaicaMon X',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
